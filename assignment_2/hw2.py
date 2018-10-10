1a12499edc3b9ea022f8e577a145cfb687312e94
yiliang6 <yiliang6@illinois.edu>
2018-10-10 12:27:49 -0500

    finished the easy lyrics word count

--- assignment_2/hw2.py
+++ assignment_2/hw2.py
@@ -1,12 +1,26 @@
 import requests
 import re
 import matplotlib.pyplot as plt
+#import apikey
+import numpy as np
 
 
 def lyrics_word_count_easy(artist, song, phrase):
-    pass
+    artist_lower = artist.lower()
+    song_lower = song.lower()
+    count = []
+    get = requests.get("https://api.lyrics.ovh/v1/" + artist_lower + "/" + song_lower)
+    if get.status_code != 200:
+        return -1
+    lyrics = get.json()["lyrics"]
+    count = re.findall(phrase, lyrics, re.IGNORECASE)
+    return len(count)
+
+print(lyrics_word_count_easy("Rick Astley", "Never Gonna Give You Up", "never"))
+
 
 def lyrics_word_count(artist, phrase):
+    
     pass
 
 def visualize():
